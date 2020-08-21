d5a3e431eebf5d31d700d4e10d7bd80ae03aebd2
Homero74 <homero_655@hotmail.com>
2020-08-21 04:22:19 -0700

    'clase_3'

--- Assets/Scripts/NewPlayer.cs
+++ Assets/Scripts/NewPlayer.cs
@@ -1,12 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class NewPlayer : MonoBehaviour
 {
     [SerializeField]
+    TextMeshProUGUI textMesh;
+
+    [SerializeField]
     float moveSpeed = 2f;
     Animator anim;
+int score;
+    [SerializeField]
+    GameObject Sonido;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -16,12 +24,41 @@ public class NewPlayer : MonoBehaviour
     void Update()
     {
         Movement();
-        anim.SetFloat("movement", Mathf.Abs(Axis.magnitude));
+        anim.SetFloat("movement", AxisMagnitudeAbs);
     }
     void Movement()
     {
-        transform.Translate( Axis * Time.deltaTime * moveSpeed);
+        if(IsMoving)
+        {
+            transform.Translate( Vector3.forward * Time.deltaTime * moveSpeed);
+            transform.rotation = Quaternion.LookRotation(Axis.normalized);
+        }
     }
+    /// <summary>
+    /// Retunrs the axis with H input and V Input.
+    /// </summary>
+    /// <returns></returns>
     Vector3 Axis => new Vector3(Input.GetAxis("Horizontal"), 0f, Input.GetAxis("Vertical"));
 
+    /// <summary>
+    /// Check if player is moving with inputs H and V.
+    /// </summary>
+    bool IsMoving => AxisMagnitudeAbs > 0;
+
+    /// <summary>
+    /// Returns the magnitude of the Axis with inputs H and V.
+    /// </summary>
+    /// <returns></returns>
+    float AxisMagnitudeAbs => Mathf.Abs(Axis.magnitude);
+    
+    void OnTriggerEnter(Collider other) 
+    {
+        if(other.CompareTag("collectable"))
+        {
+            Instantiate(Sonido);
+            score++;
+            textMesh.text = $"Score: {score}";
+            Destroy(other.gameObject);
+        }   
+    }
 }
---
+++ Assets/Scripts/SoundTime.cs
@@ -0,0 +1,23 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundTime : MonoBehaviour
+{
+    
+    [SerializeField]
+    float lifeTime=9f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Destroy(gameObject, lifeTime);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
